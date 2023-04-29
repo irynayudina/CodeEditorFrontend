98e53bba0a9e94a004bb91434878487798702638
Iryna Yudina <yudinaira4444@gmail.com>
2023-04-29 21:45:00 +0300

    userpage people section layout

--- src/pages/Utilities/UserPeople/UserPeople.jsx
+++ src/pages/Utilities/UserPeople/UserPeople.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Row, Col, Form, Badge } from "react-bootstrap";
 import './UserPeople.scss'
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import {
+  BsFillTrashFill,
+} from "react-icons/bs";
 const UserPeople = () => {
+  const [people, setPeople] = useState([
+    { name: "person 111111111111", role: "follower" },
+    { name: "person 2", role: "follower" },
+    { name: "person3", role: "follower" },
+    { name: "person4", role: "following" },
+    { name: "person5", role: "follower" },
+    { name: "person6", role: "following" },
+    { name: "person7", role: "follower" },
+  ]);
   return (
     <div className="peopleContainer">
       <div className="title-people">
@@ -16,6 +30,24 @@ const UserPeople = () => {
           <option value="1">Following</option>
           <option value="2">Followers</option>
         </Form.Select>
+      </div>
+      <div className="people-display">
+        {people.map((p, i) => (
+          <div className="person-item" key={i}>
+            <div className="person-info-group">
+              <Link to="/editor">
+                <div className="person-img"></div>
+              </Link>
+              <Link to="/editor">
+                <div className="person-name">{p.name}</div>
+                <Badge bg="secondary">{p.role}</Badge>
+              </Link>
+            </div>
+            <Button variant="outline-danger" size="sm" className="btn-center">
+              <BsFillTrashFill />
+            </Button>
+          </div>
+        ))}
       </div>
     </div>
   );
