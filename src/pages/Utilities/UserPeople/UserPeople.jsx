bc33851ee0ca0aa5421de72ce3ba5028038cdae2
Iryna Yudina <yudinaira4444@gmail.com>
2023-04-29 23:02:58 +0300

    sorting and deleting peope from a user

--- src/pages/Utilities/UserPeople/UserPeople.jsx
+++ src/pages/Utilities/UserPeople/UserPeople.jsx
@@ -7,15 +7,36 @@ import {
   BsFillTrashFill,
 } from "react-icons/bs";
 const UserPeople = () => {
-  const [people, setPeople] = useState([
-    { name: "person 111111111111", role: "follower" },
+  const peopleArray = [
+    { name: "person 11111111111111111111", role: "follower" },
     { name: "person 2", role: "follower" },
     { name: "person3", role: "follower" },
     { name: "person4", role: "following" },
     { name: "person5", role: "follower" },
     { name: "person6", role: "following" },
     { name: "person7", role: "follower" },
-  ]);
+  ];
+  const [people, setPeople] = useState(peopleArray);
+  const [filterParam, setFilter] = useState('0')
+  const deletePersonHandler = (i) => {
+    let newList = [...people];
+    newList.splice(i, 1);
+    setPeople(newList);
+    console.log(people)
+  };
+  const sortPeople = () => {
+    let filtered = peopleArray;
+
+    if (filterParam !== "0") {
+      filtered = filtered.filter(
+        (person) => person.role === filterParam
+      );
+    }
+    setPeople(filtered)
+  }
+   React.useEffect(() => {
+     sortPeople();
+   }, [filterParam]);
   return (
     <div className="peopleContainer">
       <div className="title-people">
@@ -23,12 +44,12 @@ const UserPeople = () => {
       </div>
       <div className="people-topsection">
         <Form.Select
-        // value={sortProjects}
-        // onChange={(e) => setSortProjects(e.target.value)}
+        value={filterParam}
+        onChange={(e) => setFilter(e.target.value)}
         >
           <option value="0">All</option>
-          <option value="1">Following</option>
-          <option value="2">Followers</option>
+          <option value="following">Following</option>
+          <option value="follower">Followers</option>
         </Form.Select>
       </div>
       <div className="people-display">
@@ -40,10 +61,17 @@ const UserPeople = () => {
               </Link>
               <Link to="/editor">
                 <div className="person-name">{p.name}</div>
-                <Badge bg="secondary">{p.role}</Badge>
+                <Badge bg="secondary" className="bage-role">
+                  {p.role}
+                </Badge>
               </Link>
             </div>
-            <Button variant="outline-danger" size="sm" className="btn-center">
+            <Button
+              variant="outline-danger"
+              size="sm"
+              className="btn-center"
+              onClick={() => deletePersonHandler(i)}
+            >
               <BsFillTrashFill />
             </Button>
           </div>
