96501591c160d4c1cbb23a6a8cba690186b4215d
Iryna Yudina <yudinaira4444@gmail.com>
2023-05-02 23:31:38 +0300

    some work on challemges layout on a user pagre

--- src/pages/Utilities/UserChallenges/UserChallengeList/UserChallengeList.jsx
+++ src/pages/Utilities/UserChallenges/UserChallengeList/UserChallengeList.jsx
@@ -11,14 +11,30 @@ const UserChallengeList = ({ challenges }) => {
     <div className="challenges-user-list">
       {challenges.map((challenge, index) => (
         <div key={index} className={`status-${challenge.status.toLowerCase()}`}>
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="challenge-user-item">
+          <div className="challenge-user-item">
+            {/* <div className="challenge-user-item">
               <BsFillCheckCircleFill className="mr-2" />
               {challenge.name}
               {challenge.difficulty}
+            </div> */}
+            <div className="left">
+              <div className="left-top">
+                <div className="status-challenge">status</div>
+                <div className="challenge-name">challenge name</div>
+              </div>
+              <div className="left-bottom text-muted">
+                <div className="cases-passed">8 of 8</div>
+                <div className="points-earned">points earned</div>
+                <div className="time-taken">time taken</div>
+              </div>
             </div>
-            <div>{challenge.difficulty}</div>
-            <div>{challenge.topic}</div>
+            <div className="right">
+              <div className="top-right">
+                <div>{challenge.difficulty}</div>
+                <div>{challenge.topic}</div>
+              </div>
+              <div className="date-challenge text-muted">date</div>
+            </div>
           </div>
         </div>
       ))}
