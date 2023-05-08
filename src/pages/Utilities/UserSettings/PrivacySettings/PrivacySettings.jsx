6326196816a343fbfde0f97e27080740345017ee
Iryna Yudina <yudinaira4444@gmail.com>
2023-05-08 18:27:09 +0300

    finished settings

--- src/pages/Utilities/UserSettings/PrivacySettings/PrivacySettings.jsx
+++ src/pages/Utilities/UserSettings/PrivacySettings/PrivacySettings.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import './PrivacySettings.scss'
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+
 const PrivacySettings = () => {
   return (
-      <div className='privacy-settings'>
-          <div className="ratings">progress charts visible</div>
-          <div className="social-links-privacy">social links visible</div>
-          <div className="contact-privacy">contact info visible</div>
+    <div className="privacy-settings">
+      <div className="text-settings">
+        <div className="description-setting">
+          <span>Set time for session expiration:</span>
+        </div>
+        <div className="controls-setting">
+          <Form.Control
+            type="text"
+            placeholder="Enter new time for session expiration"
+          />
+          <Button variant="primary" size="sm">
+            Save changes
+          </Button>
+        </div>
+      </div>
+      <div className="ratings">progress charts visible</div>
+      <div className="social-links-privacy">social links visible</div>
+      <div className="contact-privacy">contact info visible</div>
     </div>
-  )
+  );
 }
 
 export default PrivacySettings
--- src/pages/Utilities/UserSettings/SecuritySettings/SecuritySettings.jsx
+++ src/pages/Utilities/UserSettings/SecuritySettings/SecuritySettings.jsx
@@ -1,15 +1,69 @@
 import React from 'react'
-
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import  './SecuritySettings.scss'
 const SecuritySettings = () => {
   return (
-      <div className='security-settings'>
-          <div className="login-phone">login phone</div>
-          <div className="login-email">login email</div>
-          <div className="secon-email">second email</div>
-          <div className="token-time">set time for session expiration</div>
-          <div className="password-change">change password</div>
+    <div className="security-settings">
+      <div className="text-settings">
+        <div className="description-setting">
+          <span>Login phone:</span>
+        </div>
+        <div className="controls-setting">
+          <Form.Control type="text" placeholder="Enter new login phone" />
+          <Button variant="primary" size="sm">
+            Save changes
+          </Button>
+        </div>
+      </div>
+      <div className="text-settings">
+        <div className="description-setting">
+          <span>Login email:</span>
+        </div>
+        <div className="controls-setting">
+          <Form.Control type="text" placeholder="Enter new login email" />
+          <Button variant="primary" size="sm">
+            Save changes
+          </Button>
+        </div>
+      </div>
+      <div className="text-settings">
+        <div className="description-setting">
+          <span>Second email:</span>
+        </div>
+        <div className="controls-setting">
+          <Form.Control type="text" placeholder="Enter new second email" />
+          <Button variant="primary" size="sm">
+            Save changes
+          </Button>
+        </div>
+      </div>
+      <div className="text-settings">
+        <div className="description-setting">
+          <span>Change password:</span>
+        </div>
+        <div className="controls-setting">
+          <Form.Control type="text" placeholder="Enter new password" />
+          <Button variant="primary" size="sm">
+            Save changes
+          </Button>
+        </div>
+      </div>
+      <div className="text-settings">
+        <div className="description-setting">
+          <span>Set time for session expiration:</span>
+        </div>
+        <div className="controls-setting">
+          <Form.Control
+            type="text"
+            placeholder="Enter new time for session expiration"
+          />
+          <Button variant="primary" size="sm">
+            Save changes
+          </Button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SecuritySettings
