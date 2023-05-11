bab72550e273350bbf99953597ab30f2997680e8
Iryna Yudina <yudinaira4444@gmail.com>
2023-05-12 00:25:14 +0300

    challenge task layout with dummy data

--- src/pages/Challenge/Challenge.jsx
+++ src/pages/Challenge/Challenge.jsx
@@ -1,85 +1,70 @@
 import React, { useState } from "react";
-import { Resizable, ResizableBox, ResizeObserver } from "react-resizable";
-import "react-resizable/css/styles.css";
 import './Challenge.scss'
-
+import ResizingWrapper from "./ResizingWrapper";
+import { Card, Badge, Button, ListGroup } from "react-bootstrap";
 const Challenge = () => {
-  function convertVhToPx(valueInVh) {
-    const vh = Math.max(
-      document.documentElement.clientHeight || 0,
-      window.innerHeight || 0
-    );
-    const valueInPx = (valueInVh / 100) * vh;
-    return valueInPx;
-  }
-  function vwToPx(valueInVw) {
-    const pixelValue = (valueInVw / 100) * window.innerWidth;
-    return pixelValue;
-  }
-  const horizontalLayoutOffset = 58;
-  const section1Height = convertVhToPx(100) - horizontalLayoutOffset;
-  const section23Widths = vwToPx(50);
-  const section2HeightInit = (convertVhToPx(100) - horizontalLayoutOffset) * 0.75;
-  const section3HeightInit = (convertVhToPx(100) - horizontalLayoutOffset) * 0.25;
-  const [section1Width, setSection1Width] = useState(section23Widths);
-  const [section2Width, setSection2Width] = useState(section23Widths);
-  const [section3Width, setSection3Width] = useState(section23Widths);
-  const [section2Height, setSection2Height] = useState(section2HeightInit)
-  const [section3Height, setSection3Height] = useState(section3HeightInit);
-
-  const handleSection1Resize = (event, { size }) => {
-    const newSection1Width = size.width;
-    setSection1Width(newSection1Width);
-    const remainingWidth = window.innerWidth - newSection1Width;
-    const newSection23Width = remainingWidth;
-    setSection2Width(newSection23Width);
-    setSection3Width(newSection23Width);
+  const data = {
+    name: "String Reversal",
+    difficulty: "Easy",
+    author: "John Smith",
+    creationDate: "May 1, 2023",
+    description:
+      "Given a string, write a function to reverse it. For example, the input string 'hello' should return 'olleh'.",
+    topic: "Strings",
+    tags: ["reverse", "string manipulation"],
+    examples: [
+      { input: "hello", output: "olleh" },
+      { input: "coding is fun", output: "nuf si gnidoc" },
+      { input: "", output: "" },
+    ],
+    points: 10,
+    discussionCount: 5,
   };
 
-  const handleSection2Resize = (event, { size }) => {
-    setSection2Width(section2Width);
-    const newSection2Height = size.height;
-    setSection2Height(size.height);
-    const remainingHeight = window.innerHeight - newSection2Height - horizontalLayoutOffset;
-    setSection3Height(remainingHeight);
-  };
-  
   return (
-    <div>
-      <div className="challenge-page">
-        <ResizableBox
-          width={section1Width}
-          height={section1Height}
-          minConstraints={[200, section1Height]}
-          maxConstraints={[1400, section1Height]}
-          onResize={handleSection1Resize}
-          className="challenge-section"
-        >
-          <div>Section 1</div>
-        </ResizableBox>
-        <div className="right-side-challenge-layout">
-          <ResizableBox
-            width={section2Width}
-            height={section2Height}
-            minConstraints={[section2Width, 100]}
-            maxConstraints={[section2Width, section1Height]}
-            onResize={handleSection2Resize}
-            className="challenge-section"
-          >
-            <div>Section 2</div>
-          </ResizableBox>
-          <ResizableBox
-            width={section3Width}
-            height={section3Height}
-            minConstraints={[section3Width, section3Height]}
-            maxConstraints={[section3Width, section3Height]}
-            className="challenge-section"
-          >
-            <div>Section 3</div>
-          </ResizableBox>
-        </div>
+    <ResizingWrapper>
+      <div>
+        <Card>
+          <Card.Header>
+            <h3>{data.name}</h3>
+            <Badge variant="success">{data.difficulty}</Badge>
+          </Card.Header>
+          <Card.Body>
+            <Card.Subtitle className="mb-2 text-muted">
+              {data.author} • {data.creationDate}
+            </Card.Subtitle>
+            <Card.Text>{data.description}</Card.Text>
+            <Card.Subtitle className="mb-2">Topic: {data.topic}</Card.Subtitle>
+            <Card.Subtitle className="mb-2">Tags:</Card.Subtitle>
+            <ListGroup horizontal>
+              {data.tags.map((tag, index) => (
+                <ListGroup.Item key={index}>{tag}</ListGroup.Item>
+              ))}
+            </ListGroup>
+            <hr />
+            <Card.Title>Examples:</Card.Title>
+            {data.examples.map((example, index) => (
+              <div key={index}>
+                <h5>Example {index + 1}:</h5>
+                <Card.Text>
+                  Input: {example.input}
+                  <br />
+                  Expected output: {example.output}
+                </Card.Text>
+              </div>
+            ))}
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">
+              Points: {data.points} • Discussion: {data.discussionCount}
+            </small>
+            <Button variant="primary">Start Challenge</Button>
+          </Card.Footer>
+        </Card>
       </div>
-    </div>
+      <div>Code Editor</div>
+      <div>Test Cases</div>
+    </ResizingWrapper>
   );
 };
 
