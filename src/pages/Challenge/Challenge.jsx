6ccef4f79e02f9e0de8aab9f9032bffa7d1f2764
Iryna Yudina <yudinaira4444@gmail.com>
2023-05-12 22:51:40 +0300

    challenge page description

--- src/pages/Challenge/Challenge.jsx
+++ src/pages/Challenge/Challenge.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './Challenge.scss'
 import ResizingWrapper from "./ResizingWrapper";
 import { Card, Badge, Button, ListGroup } from "react-bootstrap";
+import {Container, Col, Row } from "react-bootstrap";
 const Challenge = () => {
   const data = {
     name: "String Reversal",
@@ -22,49 +23,105 @@ const Challenge = () => {
   };
 
   return (
-    <ResizingWrapper>
-      <div>
-        <Card>
-          <Card.Header>
-            <h3>{data.name}</h3>
-            <Badge variant="success">{data.difficulty}</Badge>
-          </Card.Header>
-          <Card.Body>
-            <Card.Subtitle className="mb-2 text-muted">
-              {data.author} • {data.creationDate}
-            </Card.Subtitle>
-            <Card.Text>{data.description}</Card.Text>
-            <Card.Subtitle className="mb-2">Topic: {data.topic}</Card.Subtitle>
-            <Card.Subtitle className="mb-2">Tags:</Card.Subtitle>
-            <ListGroup horizontal>
-              {data.tags.map((tag, index) => (
-                <ListGroup.Item key={index}>{tag}</ListGroup.Item>
+    <div>
+      <ResizingWrapper>
+        <div>
+          <Card>
+            <Card.Header>
+              <h3 className="d-flex justify-content-between align-items-start">
+                <div>{data.name} </div>
+                <Badge bg="secondary" className="float-right">
+                  0:51
+                </Badge>
+              </h3>
+              <Badge variant="success">{data.difficulty}</Badge>
+            </Card.Header>
+            <Card.Body>
+              <Card.Subtitle className="mb-2 text-muted">
+                {data.author} • {data.creationDate}
+              </Card.Subtitle>
+              <Card.Text>{data.description}</Card.Text>
+              <Card.Subtitle className="mb-2">
+                Topic: {data.topic}
+              </Card.Subtitle>
+              <Card.Subtitle className="mb-2">Tags:</Card.Subtitle>
+              <ListGroup horizontal>
+                {data.tags.map((tag, index) => (
+                  <ListGroup.Item size="sm" key={index}>
+                    {tag}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+              <hr />
+              <Card.Title className="text-center">Test examples</Card.Title>
+              {data.examples.map((example, index) => (
+                <div key={index}>
+                  <h5>example {index + 1}:</h5>
+                  <Card.Text>
+                    Input: {example.input}
+                    <br />
+                    Expected output: {example.output}
+                  </Card.Text>
+                </div>
               ))}
+            </Card.Body>
+            <Card.Footer className="gap-3">
+              <small className="text-muted">
+                Points: 0/{data.points} • Discussion: {data.discussionCount}{" "}
+              </small>
+              <Button variant="primary" size="sm">
+                Start Challenge
+              </Button>
+            </Card.Footer>
+          </Card>
+        </div>
+        <div>Code Editor</div>
+        <div>
+          <Card>
+            {/* <Card.Body> */}
+            <ListGroup as="ol" numbered>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Subheading</div>
+                  Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                  10
+                </Badge>
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Subheading</div>
+                  Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                  10
+                </Badge>
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Subheading</div>
+                  Cras justo odio
+                </div>
+                <Badge bg="primary" pill>
+                  10
+                </Badge>
+              </ListGroup.Item>
             </ListGroup>
-            <hr />
-            <Card.Title>Examples:</Card.Title>
-            {data.examples.map((example, index) => (
-              <div key={index}>
-                <h5>Example {index + 1}:</h5>
-                <Card.Text>
-                  Input: {example.input}
-                  <br />
-                  Expected output: {example.output}
-                </Card.Text>
-              </div>
-            ))}
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">
-              Points: {data.points} • Discussion: {data.discussionCount}
-            </small>
-            <Button variant="primary">Start Challenge</Button>
-          </Card.Footer>
-        </Card>
-      </div>
-      <div>Code Editor</div>
-      <div>Test Cases</div>
-    </ResizingWrapper>
+            {/* </Card.Body> */}
+          </Card>
+        </div>
+      </ResizingWrapper>
+    </div>
   );
 };
 
--- src/pages/Challenge/ResizingWrapper.jsx
+++ src/pages/Challenge/ResizingWrapper.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Resizable, ResizableBox, ResizeObserver } from "react-resizable";
 import "react-resizable/css/styles.css";
 // import "./Challenge.scss";
 
-const ResizingWrapper = ({children}) => {
-     const childrenArray = React.Children.toArray(children);
-     const section1 = childrenArray[0];
-     const section2 = childrenArray[1];
-     const section3 = childrenArray[2];
+const ResizingWrapper = ({children, s1, s2, s3}) => {
+  const childrenArray = React.Children.toArray(children);
+  const section1 = childrenArray[0] || s1;
+  const section2 = childrenArray[1] || s2;
+  const section3 = childrenArray[2] || s3;
   function convertVhToPx(valueInVh) {
     const vh = Math.max(
       document.documentElement.clientHeight || 0,
@@ -24,9 +24,9 @@ const ResizingWrapper = ({children}) => {
   const section1Height = convertVhToPx(100) - horizontalLayoutOffset;
   const section23Widths = vwToPx(50);
   const section2HeightInit =
-    (convertVhToPx(100) - horizontalLayoutOffset) * 0.75;
+    (convertVhToPx(100) - horizontalLayoutOffset) * 0.60;
   const section3HeightInit =
-    (convertVhToPx(100) - horizontalLayoutOffset) * 0.25;
+    (convertVhToPx(100) - horizontalLayoutOffset) * 0.40;
   const [section1Width, setSection1Width] = useState(section23Widths);
   const [section2Width, setSection2Width] = useState(section23Widths);
   const [section3Width, setSection3Width] = useState(section23Widths);
@@ -49,11 +49,29 @@ const ResizingWrapper = ({children}) => {
     const remainingHeight =
       window.innerHeight - newSection2Height - horizontalLayoutOffset;
     setSection3Height(remainingHeight);
+    // setSection3Width(section2Width);
   };
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      // calculate new section sizes based on current sizes of all sections
+      const newWidths = vwToPx(50);
+      setSection1Width(newWidths);
+      setSection2Width(newWidths);
+      setSection3Width(newWidths);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, [ ]);
 
   return (
     <div>
       <div className="challenge-page">
+        <div className="challenge-section-xs">{section1}</div>
+        <div className="challenge-section-xs">{section2}</div>
+        <div className="challenge-section-xs">{section3}</div>
         <ResizableBox
           width={section1Width}
           height={section1Height}
