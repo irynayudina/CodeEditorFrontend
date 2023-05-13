332fad258d90726abd073d0cf63270748f9ba66d
Iryna Yudina <yudinaira4444@gmail.com>
2023-05-13 14:03:45 +0300

    improoved layout of challenge page

--- src/pages/Challenge/Challenge.jsx
+++ src/pages/Challenge/Challenge.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import './Challenge.scss'
 import ResizingWrapper from "./ResizingWrapper";
-import { Card, Badge, Button, ListGroup } from "react-bootstrap";
-import {Container, Col, Row } from "react-bootstrap";
+import ChallengeInfo from "./ChallengeInfo";
+import TestsInfo from "./TestsInfo";
+import CodeEditorForChallenge from "./CodeEditorForChallenge";
 const Challenge = () => {
   const data = {
     name: "String Reversal",
@@ -25,101 +26,9 @@ const Challenge = () => {
   return (
     <div>
       <ResizingWrapper>
-        <div>
-          <Card>
-            <Card.Header>
-              <h3 className="d-flex justify-content-between align-items-start">
-                <div>{data.name} </div>
-                <Badge bg="secondary" className="float-right">
-                  0:51
-                </Badge>
-              </h3>
-              <Badge variant="success">{data.difficulty}</Badge>
-            </Card.Header>
-            <Card.Body>
-              <Card.Subtitle className="mb-2 text-muted">
-                {data.author} • {data.creationDate}
-              </Card.Subtitle>
-              <Card.Text>{data.description}</Card.Text>
-              <Card.Subtitle className="mb-2">
-                Topic: {data.topic}
-              </Card.Subtitle>
-              <Card.Subtitle className="mb-2">Tags:</Card.Subtitle>
-              <ListGroup horizontal>
-                {data.tags.map((tag, index) => (
-                  <ListGroup.Item size="sm" key={index}>
-                    {tag}
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-              <hr />
-              <Card.Title className="text-center">Test examples</Card.Title>
-              {data.examples.map((example, index) => (
-                <div key={index}>
-                  <h5>example {index + 1}:</h5>
-                  <Card.Text>
-                    Input: {example.input}
-                    <br />
-                    Expected output: {example.output}
-                  </Card.Text>
-                </div>
-              ))}
-            </Card.Body>
-            <Card.Footer className="gap-3">
-              <small className="text-muted">
-                Points: 0/{data.points} • Discussion: {data.discussionCount}{" "}
-              </small>
-              <Button variant="primary" size="sm">
-                Start Challenge
-              </Button>
-            </Card.Footer>
-          </Card>
-        </div>
-        <div>Code Editor</div>
-        <div>
-          <Card>
-            {/* <Card.Body> */}
-            <ListGroup as="ol" numbered>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-              >
-                <div className="ms-2 me-auto">
-                  <div className="fw-bold">Subheading</div>
-                  Cras justo odio
-                </div>
-                <Badge bg="primary" pill>
-                  10
-                </Badge>
-              </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-              >
-                <div className="ms-2 me-auto">
-                  <div className="fw-bold">Subheading</div>
-                  Cras justo odio
-                </div>
-                <Badge bg="primary" pill>
-                  10
-                </Badge>
-              </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-              >
-                <div className="ms-2 me-auto">
-                  <div className="fw-bold">Subheading</div>
-                  Cras justo odio
-                </div>
-                <Badge bg="primary" pill>
-                  10
-                </Badge>
-              </ListGroup.Item>
-            </ListGroup>
-            {/* </Card.Body> */}
-          </Card>
-        </div>
+        <ChallengeInfo data={data} />
+        <CodeEditorForChallenge />
+        <TestsInfo />
       </ResizingWrapper>
     </div>
   );
