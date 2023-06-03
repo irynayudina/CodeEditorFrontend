6805c04abc9e75ea393dc8fc36cd93fe31b519d0
Iryna Yudina <yudinaira4444@gmail.com>
2023-06-03 11:35:27 +0300

    filtering projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,29 +1,39 @@
-import React, {useState, useEffect} from 'react'
-import './Projects.scss'
+import React, { useState, useEffect } from "react";
+import "./Projects.scss";
 import { Container, Row, Col, Form, Badge } from "react-bootstrap";
-import { languages } from '../Editor/Syntax/EditorData.ts';
+import { languages } from "../Editor/Syntax/EditorData.ts";
 import { Button } from "react-bootstrap";
-import Topic from '../Challenges/Topics/Topic';
-import Project from '../Project/Project'
-import axios from 'axios';
+import Topic from "../Challenges/Topics/Topic";
+import Project from "../Project/Project";
+import axios from "axios";
 import { toast } from "react-toastify";
 
-import Loader from '../../elements/Loader';
+import Loader from "../../elements/Loader";
 import { ListGroup } from "react-bootstrap";
 import useInfiniteScroll from "react-infinite-scroll-hook";
 
-function useLoadItems() {
+function useLoadItems(filerObj) {
   const [loading, setLoading] = useState(false);
   const [items, setItems] = useState([]);
   const [hasNextPage, setHasNextPage] = useState(true);
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
   const [resetCompleted, setResetCompleted] = useState(false);
+
+  let filterQuery = filerObj.projectName
+    ? `&projectName=${filerObj.projectName}`
+    : "";
+  if (filerObj.language) {
+    filterQuery += `&language=${filerObj.language}`;
+  }
+
   const loadMore = async () => {
     if (!loading && hasNextPage) {
       setLoading(true);
       try {
-        const response = await axios.get(`/api/projects?page=${page}`);
+        const response = await axios.get(
+          `/api/projects?page=${page}${filterQuery}`
+        );
         setItems((prevItems) => {
           // Filter out duplicate items
           const uniqueItems = response.data.projects.filter((item) => {
@@ -46,6 +56,22 @@ function useLoadItems() {
     }
     console.log(page + " inside loadmore");
   };
+
+  const statesReset = () => {
+    setPage(1);
+    setHasNextPage(true);
+    setLoading(false);
+    setItems([]);
+    setResetCompleted(true); //--
+  };
+  useEffect(() => {
+    if (filterQuery !== "") {
+      setResetCompleted(false);
+      statesReset();
+      console.log(page + " useEffect");
+    }
+  }, [filerObj, filterQuery]);
+
   useEffect(() => {
     if (resetCompleted) {
       loadMore();
@@ -56,133 +82,118 @@ function useLoadItems() {
 }
 
 const Projects = () => {
-   const [projects, setProjects] = useState([
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "999k",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-     {
-       name: "project1",
-       language: "c++",
-       createdAt: "26.04.2023",
-       codeFile: "...",
-       likes: "10",
-     },
-   ]);
-  const [sortProjects, setSortProjects] = useState('0')
-  
+  const [projects, setProjects] = useState([
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "999k",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+    {
+      name: "project1",
+      language: "c++",
+      createdAt: "26.04.2023",
+      codeFile: "...",
+      likes: "10",
+    },
+  ]);
+  const [sortProjects, setSortProjects] = useState("0");
 
-  // const getProjectsData = async () => {
-  //   try {
-  //     const projectData = await axios.get(`/api/projects`);
-  //     if (projectData?.data) {
-  //       setProjects(projectData?.data?.projects);
-  //       console.log(projectData.data.projects);
-  //       toast.success("got projects");
-  //     }
-  //   } catch (err) {
-  //     toast.error(err?.response?.data?.message || err.error);
-  //   }
-  // };
-  // useEffect(() => {
-  //   getProjectsData();//pagination
-  // }, [])
-  
-  let { loading, items, hasNextPage, error, loadMore } = useLoadItems(); 
+  const [filerObj, setFilterObj] = useState({});
+
+  let { loading, items, hasNextPage, error, loadMore } = useLoadItems(filerObj);
   const [sentryRef] = useInfiniteScroll({
     loading,
     hasNextPage,
@@ -194,7 +205,35 @@ const Projects = () => {
   useEffect(() => {
     setProjects(items);
   }, [items]);
-  
+
+  const [name, setName] = useState("");
+  const [languagesSelected, setLanguagesSelected] = useState([]);
+  const handleLanguageChange = (event, language) => {
+    if (event.target.checked) {
+      // Add the selected language to the array
+      setLanguagesSelected([...languagesSelected, language]);
+    } else {
+      // Remove the unselected language from the array
+      setLanguagesSelected(
+        languagesSelected.filter((lang) => lang !== language)
+      );
+    }
+  };
+  const filterProjects = () => {
+    setFilterObj({
+      projectName: name,
+      language: languagesSelected,
+    });
+    console.log({
+      projectName: name,
+      language: languagesSelected,
+    });
+    // console.log(sortDiscussions);
+  };
+
+  useEffect(() => {
+    filterProjects();
+  }, []);
 
   return (
     <div className="projects-container">
@@ -204,21 +243,27 @@ const Projects = () => {
           value={sortProjects}
           onChange={(e) => setSortProjects(e.target.value)}
         >
-          <option value="0">Trending</option>
           <option value="1">Recent</option>
           <option value="2">Popular</option>
         </Form.Select>
-        <Form.Control type="text" placeholder="Search" />
-        <Button size="md">Search</Button>
+        <Form.Control
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Button size="md" onClick={filterProjects}>
+          Search
+        </Button>
       </div>
       <Topic title="Select languages" className="languages-select">
         <div className="languages">
-          {Object.keys(languages).map((key) => (
+          {Object.keys(languages).map((keyLanguage) => (
             <Form.Check
-              key={key}
+              key={keyLanguage}
               type="checkbox"
-              label={languages[key]}
-              value={languages[key]}
+              label={languages[keyLanguage]}
+              value={keyLanguage}
+              onChange={(e) => handleLanguageChange(e, keyLanguage)}
             />
           ))}
         </div>
@@ -237,6 +282,6 @@ const Projects = () => {
       )}
     </div>
   );
-}
+};
 
-export default Projects
+export default Projects;
